be6c1019af641cbfec833e6e89affd4f4f371d02
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2026-01-15 07:31:56 +0530

    add request validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/pradnyaakumbhar/students-api/internal/config"
+	"github.com/pradnyaakumbhar/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -21,9 +22,7 @@ func main() {
 	// db setup
 	// setup router
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	// setup server
 	server := http.Server{
