fc1296fe5ace2950e224d8c465f29a1223e48aaf
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2026-01-20 07:49:24 +0530

    get student by id

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -31,7 +31,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 	router.HandleFunc("POST /api/students", student.New(storage))
-	// router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
 
 	// setup server
 	server := http.Server{
